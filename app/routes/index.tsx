35a9b6e89f600be3e7c34d63c15506b36baab700
Patryk Błaziński <blazinski.patryk@gmail.com>
2022-09-25 20:50:09 +0200

    feat: 🎸 add prisma tailwindcss and simple user route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,22 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  let res = await fetch("http://localhost:3000/api/users");
+  let users: any[] = await res.json();
+  return json(users);
+}
+
 export default function Index() {
+  const users = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1 className="text-3xl font-light">Welcome to Remix</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.email} - {user.name}
+        </div>
+      ))}
     </div>
   );
 }
