9b9d00f696d10e3f3578c1994fa703067b443621
Patryk Błaziński <blazinski.patryk@gmail.com>
2022-09-26 19:49:33 +0200

    feat: create user

--- app/routes/api/users/index.ts
+++ app/routes/api/users/index.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { ActionFunction, json } from "@remix-run/node";
 
 const prisma = new PrismaClient();
 
 export async function loader() {
-  console.log("it was called");
-
   const users = await prisma.user.findMany();
   return new Response(JSON.stringify(users), {
     status: 200,
@@ -13,3 +12,29 @@ export async function loader() {
     },
   });
 }
+
+export const action: ActionFunction = async ({ request }) => {
+  switch (request.method) {
+    case "POST": {
+      console.log("Create new user");
+      const result = await request.json();
+      console.log(result);
+      const user = await prisma.user.create({
+        data: { name: result.name, email: result.email },
+        select: { name: true, id: true, email: true },
+      });
+      return json(user, { status: 201 });
+
+      /* handle "POST" */
+    }
+    case "PUT": {
+      /* handle "PUT" */
+    }
+    case "PATCH": {
+      /* handle "PATCH" */
+    }
+    case "DELETE": {
+      /* handle "DELETE" */
+    }
+  }
+};
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 
 export async function loader() {
   let res = await fetch("http://localhost:3000/api/users");
@@ -7,16 +9,56 @@ export async function loader() {
   return json(users);
 }
 
+export const action: ActionFunction = async ({ request }) => {
+  console.log("create a new userk");
+
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const email = formData.get("email");
+
+  let res = await fetch("http://localhost:3000/api/users", {
+    method: "post",
+    body: JSON.stringify({ name, email }),
+  });
+
+  console.log(await res.json());
+
+  console.log({ name, email });
+  return redirect("/");
+};
+
 export default function Index() {
   const users = useLoaderData<typeof loader>();
   return (
     <div>
       <h1 className="text-3xl font-light">Welcome to Remix</h1>
-      {users.map((user) => (
-        <div key={user.id}>
-          {user.email} - {user.name}
-        </div>
-      ))}
+      <div>
+        <Form method="post" className="inline-flex gap-6 flex-col">
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="name">Name</label>
+            <input id="name" className="border" name="name" required />
+          </fieldset>
+
+          <fieldset className="flex flex-col gap-2">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              className="border"
+              type={"email"}
+              name={"email"}
+              required
+            />
+          </fieldset>
+          <button className="border" type="submit">
+            Add User
+          </button>
+        </Form>
+        {users.map((user) => (
+          <div key={user.id}>
+            {user.email} - {user.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
